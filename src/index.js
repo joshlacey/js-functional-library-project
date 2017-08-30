d39848d9d080d32acb5680612fd66a0f2fe35906
joshlacey <williamjoshualacey@gmail.com>
2017-08-30 13:04:55 -0400

    did some stuff

--- src/index.js
+++ src/index.js
@@ -14,50 +14,40 @@ const fi = (function() {
       }
     },
     map: function(list, iteratee) {
-      if (Array.isArray(list)){
         const newArray = []
-
         this.each(list, element => {
           newArray.push(iteratee(element))
         })
-
         return newArray
-
-      } else {
-        this.each(list, value => {
-        return 'hi' })
-      }
     },
-  }
-})()
-//   filter: function(list, iteratee) {
-//     const newArr = []
-//
-//     for (let i = 0; i < list.length; i++) {
-//       const el = list[i]
-//       if (iteratee(el)) newArr.push(el)
-//     }
-//
-//     return newArr
-//   },
-//   find: function find(list, iteratee) {
-//     let returnVal
-//     for (let i = 0; i < list.length; i++) {
-//       returnVal = list[i]
-//       if (iteratee(returnVal) === true) break
-//     }
-//
-//     return returnVal
-//   },
-//   reduce: function reduce(list, iteratee, initVal){
-//   	let startingEl = initVal ? initVal : list[0]
-//   	let i = initVal ? 0 : 1
-//
-//   	for(i; i < list.length; i++){
-//   		startingEl = iteratee(startingEl, list[i]);
-//   	}
-//   	return startingEl
-//   },
-// }
 
-// fi.libraryMethod()
+  filter: function(list, predicate) {
+      const newArr = []
+      this.each(list, element => {
+        if (predicate(element)) {
+          newArr.push(element)
+        }
+      })
+      return newArr
+    },
+  find: function find(list, predicate) {
+      let returnVal
+
+      this.each(list, element => {
+        if (predicate(element) === true && returnVal === undefined) {
+          returnVal = element
+        }
+    })
+      return returnVal
+    },
+  reduce: function reduce(list, iteratee, initVal){
+    	let startingEl = initVal ? initVal : list[0]
+    	let i = initVal ? 0 : 1
+
+    	for(i; i < list.length; i++){
+    		startingEl = iteratee(startingEl, list[i]);
+    	}
+    	return startingEl
+    },
+}
+})()
